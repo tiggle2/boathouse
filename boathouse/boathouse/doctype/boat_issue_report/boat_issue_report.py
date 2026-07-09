05ae8311f5f9d5cb2fb12b5c3a51fb1538375047
tiggle2 <tobraonain29@gmail.com>
2026-07-09 23:22:56 +0000

    Add Basic DocType

---
+++ boathouse/boathouse/doctype/boat_issue_report/boat_issue_report.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, tsob and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class BoatIssueReport(Document):
+	pass
---
+++ boathouse/boathouse/doctype/repair_job/repair_job.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, tsob and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Repair Job", {
+// 	refresh(frm) {
+
+// 	},
+// });
