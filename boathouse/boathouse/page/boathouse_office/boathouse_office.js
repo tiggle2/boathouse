47e391126d46596f30ea8d0e5d9487d65a8c3d1b
tiggle2 <tobraonain29@gmail.com>
2026-07-12 13:19:42 +0000

    small tweaks

---
+++ boathouse/boathouse/page/boathouse_office/boathouse_office.js
@@ -0,0 +1,7 @@
+frappe.pages['boathouse-office'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'Boathouse Office',
+		single_column: true
+	});
+}
