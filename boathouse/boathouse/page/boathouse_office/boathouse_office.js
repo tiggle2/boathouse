d6d8b4d6088ac081f966a781290e05c2b4bf1ca0
tiggle2 <tobraonain29@gmail.com>
2026-07-12 16:29:05 +0100

    Load office.js and office.css on boathouse office page
    Added dependencies for office.js and office.css to boathouse office page.

--- boathouse/boathouse/page/boathouse_office/boathouse_office.js
+++ boathouse/boathouse/page/boathouse_office/boathouse_office.js
@@ -1,7 +1,18 @@
-frappe.pages['boathouse-office'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Boathouse Office',
-		single_column: true
-	});
-}
+frappe.pages["boathouse-office"].on_page_load = function(wrapper) {
+
+    let page = frappe.ui.make_app_page({
+        parent: wrapper,
+        title: "Boathouse Office",
+        single_column: true
+    });
+
+    frappe.require([
+        "/assets/boathouse/js/boathouse_office/office.js",
+        "/assets/boathouse/css/boathouse_office/office.css"
+    ], () => {
+
+        boathouse_office.init(page);
+
+    });
+
+};
