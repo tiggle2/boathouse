47986ec22113d7973bbb19f68548815310ff2837
tiggle2 <tobraonain29@gmail.com>
2026-07-12 20:45:07 +0100

    Add JavaScript inclusion for boathouse app

--- boathouse/hooks.py
+++ boathouse/hooks.py
@@ -4,6 +4,9 @@ app_publisher = "tsob"
 app_description = "Manages Bookings and Maintenance"
 app_email = "tobraonain29@gmail.com"
 app_license = "mit"
+app_include_js = [
+    "/assets/boathouse/js/boathouse_office.bundle.js"
+]
 fixtures = [
     "Custom Field",
     "Property Setter",
