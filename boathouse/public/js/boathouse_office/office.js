6808db651a69a31989251a47fa670ebd92d8e56b
tiggle2 <tobraonain29@gmail.com>
2026-07-12 22:08:18 +0100

    Refactor office.js to implement BoatHouseCalendar
    Removed the old office initialization and rendering logic. Added a new BoatHouseCalendar object with an init method to load calendar functionality.

--- boathouse/public/js/boathouse_office/office.js
+++ boathouse/public/js/boathouse_office/office.js
@@ -1,87 +1,15 @@
-window.boathouse_office = {
+console.log("BOATHOUSE OFFICE PUBLIC JS LOADED");
 
-    init(page) {
-
-        this.page = page;
-
-        this.render();
-
-    },
-
-
-    render() {
-
-        this.page.main.html(`
-
-            <div class="boathouse-office">
-
-                <div class="office-toolbar">
-                    <button class="btn btn-primary" id="new-crane">
-                        New Crane Booking
-                    </button>
-
-                    <button class="btn btn-success" id="new-rental">
-                        New Rental Booking
-                    </button>
-                </div>
-
-
-                <div class="office-layout">
-
-                    <div id="office-calendar"></div>
-
-
-                    <div class="office-sidebar">
-
-                        <h4>
-                            Outstanding Payments
-                        </h4>
-
-                        <div id="payments">
-                            Loading...
-                        </div>
-
-                    </div>
-
-                </div>
-
+window.BoathouseCalendar = {
+    init(element) {
+        element.innerHTML = `
+            <div style="
+                padding:20px;
+                background:#dcfce7;
+                border:2px solid green;
+            ">
+                Calendar JS loaded
             </div>
-
-        `);
-
-
-        this.bind_events();
-
-
-        frappe.require([
-            "/assets/boathouse/js/boathouse_office/calendar.js",
-            "/assets/boathouse/js/boathouse_office/sidebar.js"
-        ], () => {
-
-            boathouse_calendar.init();
-
-            boathouse_sidebar.init();
-
-        });
-
-    },
-
-
-    bind_events() {
-
-        $("#new-crane").click(() => {
-
-            frappe.new_doc("Crane Booking");
-
-        });
-
-
-        $("#new-rental").click(() => {
-
-            frappe.new_doc("Rental Booking");
-
-        });
-
+        `;
     }
-
 };
