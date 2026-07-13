a521cbe21d37b21b007147838d73eda9aab56811
tiggle2 <tobraonain29@gmail.com>
2026-07-13 12:18:09 +0100

    Initialize calendar with FullCalendar plugins

--- boathouse/public/js/boathouse_office/calendar.js
+++ boathouse/public/js/boathouse_office/calendar.js
@@ -1,1 +1,28 @@
+import { Calendar } from "@fullcalendar/core";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
 
+
+export function init_calendar(element) {
+
+    const calendar = new Calendar(element, {
+
+        plugins: [
+            dayGridPlugin,
+            timeGridPlugin
+        ],
+
+        initialView: "timeGridWeek",
+
+        editable: false,
+
+        height: "auto",
+
+        events: []
+
+    });
+
+
+    calendar.render();
+
+}
